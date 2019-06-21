c6b2f837161f4db55663e44b7692f5e04ece7141
lumin <527724134@qq.com>
2019-06-21 15:11:50 +0800

    feat:  添加 log warn

--- utils/log.js
+++ utils/log.js
@@ -4,7 +4,11 @@ const log = console.log;
 function error(msg) {
   log(chalk.red(`! ${msg}`));
 }
+function warn(msg) {
+  log(chalk.yellow(`! ${msg}`));
+}
 
 module.exports = {
-  error
+  error,
+  warn
 };
