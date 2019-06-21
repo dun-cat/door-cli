570e97158ab748faa393ddd6be401a613ee28173
lumin <527724134@qq.com>
2019-06-22 01:25:19 +0800

    temp:  临时提交

--- bin/index.js
+++ bin/index.js
@@ -1,30 +1,17 @@
 #!/usr/bin/env node
-
 const requireDir = require('require-dir');
-
-const { prompt, registerPrompt } = require('inquirer');
-const program = require('commander');
 const uiConfig = requireDir('../ui-configs');
-const actions = requireDir('../actions');
-const cwd = require('../utils/cwd');
-const ora = require('ora');
+const commander = require('../lib/command');
+const { prompt, registerPrompt } = require('inquirer');
+const runner = require('../lib/runner');
+const log = require('../utils/log');
 
 registerPrompt('fuzzypath', require('inquirer-fuzzy-path'));
 registerPrompt('autocomplete', require('inquirer-autocomplete-prompt'));
 
-program.version('0.0.1');
-program.parse(process.argv);
-
-if (program.args.length === 0) {
+commander.init();
+if (commander.argsLength() === 0) {
   createUI();
-}
-
-function createCommand() {
-  // git
-  program
-    .command('install', '安装依赖包')
-    .alias('c')
-    .action();
 }
 
 async function createUI() {
@@ -32,25 +19,13 @@ async function createUI() {
     let step1 = await prompt(uiConfig.menu);
     switch (step1.answer) {
       case 'commit':
-        if (actions.git.hasProjectGit(cwd.get())) {
-          // commit
-          let step2 = await prompt(uiConfig.git.commit);
-          await actions.git.commit(step2);
-          // push
-          let step3 = await prompt(uiConfig.git.push);
-          if (step3.next) {
-            const spinner = ora('推送中...').start();
-            await actions.git.push();
-            spinner.succeed('推送成功');
-            return;
-          }
-        }
+        runner.commit();
         break;
       case 'standard':
         actions.standard.setup();
         break;
     }
   } catch (error) {
-    console.log(error);
+    log.error(error);
   }
 }
